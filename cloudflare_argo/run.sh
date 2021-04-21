2d1bf210daa9dad9524981e0402479433fab9953
Panjamapong Sermsawatsri <panjmp@gmail.com>
2021-04-21 21:55:35 +0700

    🐛 remove config file append

--- cloudflare_argo/run.sh
+++ cloudflare_argo/run.sh
@@ -2,7 +2,6 @@
 
 echo $(bashio::config 'credential_file_content') > /etc/cloudflared/credentials.json
 echo $(bashio::config 'config_file_content_base64') | base64 -d > /etc/cloudflared/config.yml
-echo "\ncredentials-file: /etc/cloudflared/credentials.json\n" >> /etc/cloudflared/config.yml
 
 bashio::log.info "Running cloudflare tunnel..."
 
