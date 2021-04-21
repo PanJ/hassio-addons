cd9ceb5391f2e37b864814260a579810795c2635
Panjamapong Sermsawatsri <panjmp@gmail.com>
2021-04-21 21:28:37 +0700

    🐛 fix run file

--- cloudflare_argo/run.sh
+++ cloudflare_argo/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bashio
 
-cat $(bashio::config 'credential_file_content') > /etc/cloudflared/credentials.json
-cat $(bashio::config 'config_file_content') > /etc/cloudflared/config.yml
+echo $(bashio::config 'credential_file_content') > /etc/cloudflared/credentials.json
+echo $(bashio::config 'config_file_content') > /etc/cloudflared/config.yml
 echo "\ncredentials-file: /etc/cloudflared/credentials.json\n" >> /etc/cloudflared/config.yml
 
 bashio::log.info "Running cloudflare tunnel..."
