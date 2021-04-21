d7d6c8f270a0320a1804dd875cea42b1951cdf2d
Panjamapong Sermsawatsri <panjmp@gmail.com>
2021-04-21 21:37:55 +0700

    🐛 Change yaml config to base64

--- cloudflare_argo/run.sh
+++ cloudflare_argo/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bashio
 
 echo $(bashio::config 'credential_file_content') > /etc/cloudflared/credentials.json
-echo $(bashio::config 'config_file_content') > /etc/cloudflared/config.yml
+echo $(bashio::config 'config_file_content_base64') | base64 -d > /etc/cloudflared/config.yml
 echo "\ncredentials-file: /etc/cloudflared/credentials.json\n" >> /etc/cloudflared/config.yml
 
 bashio::log.info "Running cloudflare tunnel..."
