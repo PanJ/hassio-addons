e8bacdb58afac26ab5f05922a74f932b8fd5b74c
teampat <teampat@gmail.com>
2021-09-01 04:51:42 +0700

    Run cloudflared from binary file

--- cloudflare_argo/run.sh
+++ cloudflare_argo/run.sh
@@ -1,8 +1,10 @@
-#!/usr/bin/env bashio
+#!/usr/bin/with-contenv bashio
+
+mkdir -p /etc/cloudflared
 
 echo $(bashio::config 'credential_file_content') > /etc/cloudflared/credentials.json
 echo $(bashio::config 'config_file_content_base64') | base64 -d > /etc/cloudflared/config.yml
 
 bashio::log.info "Running cloudflare tunnel..."
 
-cloudflared tunnel run --force
+cloudflared tunnel --config /etc/cloudflared/config.yml run --force
